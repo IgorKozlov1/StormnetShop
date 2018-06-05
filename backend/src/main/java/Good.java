bfb4de3a94a4f55cab788d1b1638f76de0cfa658
Igor <ik19890104@gmail.com>
2018-06-05 22:00:29 +0300

    Initial commit

---
+++ backend/src/main/java/Good.java
@@ -0,0 +1,14 @@
+class Good {
+    public String name;
+    public int count;
+    public int price;
+
+        public Good(){};
+
+    public Good(int count, String name, int price) {
+        this.count = count;
+        this.name=name;
+        this.price=price;
+    }
+
+}
